9882ac85eda20506f52d4017a74fbb7bd7c102bf
GameR <nikkokor@yandex.ru>
2021-10-22 19:39:20 +1000

    interface logbook

--- templates/logbook/index.js
+++ templates/logbook/index.js
@@ -1,10 +1,18 @@
 import axios from "axios";
 
-export function putReturn(data = {}) {
+    function putReturn(data = {}) {
     return axios({
         method: 'POST',
         url: "http://195.140.146.82/logbook/return",
         headers: "'Content-Type': 'application/json'",
         data: data
     });
+
+    function put(u_id, b_id) {
+        let body = {
+            user_id: u_id,
+            book_id: b_id
+        };
+        return JSON.stringify(body);
+    }
 }
