df93d68fbd38398465c09b6af8d8ef94c97b66a0
NikKokor <nikkokor@yandex.ru>
2022-01-27 16:07:28 +1000

    add test controller

---
+++ src/Controller/TestController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class TestController extends ApiController
+{
+    /**
+     * @Route("/test", name="test")
+     */
+    public function index(): JsonResponse
+    {
+        return $this->response([
+            'message' => 'old test!'
+        ], []);
+    }
+}
